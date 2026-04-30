4cacdc28323058a3a7643fd53cb34c922ad89675
svetakhachatryan <skhachatryan034@gmail.com>
2026-04-30 14:42:56 +0400

    svetakhachatryan

--- classroom/javascript/script.js
+++ classroom/javascript/script.js
@@ -1,16 +1,9 @@
-class Mardik {
-    constructor (anun, tariq){
-        this.anun = anun
-        this.tariq = tariq
-        this.azganun = "khachhatryan"
-    }
-    nerkayanal (){
-        return `es ${this.anun}n em, ${this.tariq} tarekan, azganuns ${this.azganun}`
+class Trchun {
+    constructor (tesak){
+        this.tesak
     }
 }
 
-const manan = new Mardik ("manan", 21, "khachatryan")
-const sv = new Mardik ("sv", 16, "khachatryan")
-const sofi = new Mardik ("sofi", 13, "khachatryan")
-
-console.log(sv.nerkayanal())     
+class Hav extends Trchun{
+    
+}
