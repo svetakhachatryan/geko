2eb1a657b60ce59deb7e9f0da53f0cdffc58f630
svetakhachatryan <skhachatryan034@gmail.com>
2026-05-09 13:38:55 +0400

    svetakhachatryan

---
+++ classroom/react/src/components/Counter/Counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+      const [count, setCount] = useState(0)
+  return (
+    <div>
+        <h1>Count: {count}</h1>
+        <div className='btns'>
+            <button onClick={()=> setCount(count + 1)}>+</button>
+            <button onClick={()=> setCount(count - 1)}>-</button>
+        </div>
+    </div>
+  )
+}
+
+export default Counter 
