92cb2c217d10179a60cf44769dba4710198de930
svetakhachatryan <skhachatryan034@gmail.com>
2026-06-16 01:46:13 +0400

    svetakhachatryan

---
+++ homework/23_Python Intro/script.py
@@ -0,0 +1,16 @@
+name = input("Enter your name ")
+surname = input("Enter your surname ")
+age = int(input("Enter your age "))
+
+print(f"Անուն: {name}")
+print(f"Ազգանուն: {surname}")
+print(f"Տարիք: {age}")
+
+daily_salary = int(input("Enter your daily_salary "))
+days = int(input("Enter your days "))
+print(daily_salary * days)
+
+password = input("Enter password ")
+if len (password) >= 8:
+  print("Վավեր գաղտնաբառ")
+
